7d470b819ea0bd83dd20e3d14eb7c19dd3354698
Michael S <Michael.Stojanovic@hotmail.com>
2023-04-23 15:02:57 +0200

    added user ticket models

---
+++ Models/CreateModels/CreateUserTicketEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagementAPI.Models.CreateModels
+{
+    public class CreateUserTicketEntity
+    {
+        public int UserId { get; set; }
+        public int TicketId { get; set; }
+
+    }
+}
