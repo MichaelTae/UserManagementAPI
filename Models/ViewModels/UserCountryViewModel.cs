ebaec755486b006b8850b98879a28f22f5c4a450
Michael S <Michael.Stojanovic@hotmail.com>
2023-05-08 13:46:25 +0200

    added metrics controller and viewModels

---
+++ Models/ViewModels/UserCountryViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace UserManagementAPI.Models.ViewModels
+{
+    public class UserCountryViewModel
+    {
+        public UserCountryViewModel()
+        {
+            
+        }
+
+        public UserCountryViewModel(string country, int quantity)
+        {
+            Country = country;
+            Quantity = quantity;
+        }
+        public string Country { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
